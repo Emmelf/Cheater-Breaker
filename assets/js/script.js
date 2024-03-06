e45d75a9eeae6fc87aaf3265766527eea5b0eb5b
antonin.mey <antoninmeynadier@gmail.com>
2024-03-06 16:59:09 +0100

    Début de la fonction détection de sortie de page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,18 +6,26 @@ function handleWindowResize() {
     alert("NON NON NON, REDUIT PAS MON QUIZZ !!!");
   }
   
-  document.addEventListener("DOMContentLoaded", function () {
-    generatePage(pageIndex);
-  });
-  
-  window.addEventListener('resize', handleWindowResize);
+document.addEventListener("DOMContentLoaded", function () {
+  generatePage(pageIndex);
+});
+
+window.addEventListener('resize', handleWindowResize);
 
 
 //Anti clique droit.
 
 document.oncontextmenu = document.body.oncontextmenu = function() {return false;}
 
-//Anti changement de fenêtre
+//Anti changement de fenêtre 
 
+const mouseTarget = document.getElementById('page');
+
+mouseTarget.addEventListener('mouseleave', e => {
+   /* on soustrait le defilement de la page pour obtenir la vraie position du curseur */
+   if (e.pageY - window.scrollY <= 0) {
+      console.log("L'utilisateur tente de sortir de la page.");
+   }
+});
 
 //Anti copier coller
