eec5f725d804fae552a41c1395fea61145d4c6fe
Mika75020213 <lahloumikaelndl@gmail.com>
2024-03-06 16:07:06 +0100

    javascript_sent

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
 console.log('hello world');
+
+
+function handleWindowResize() {
+    alert("NON NON NON, REDUIT PAS MON QUIZZ !!!");
+  }
+  
+  function handleRightClick(event) {
+    event.preventDefault();
+    alert("CLIQUE DROIT DE QUOI TOI ?");
+  }
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    generatePage(pageIndex);
+  });
+  
+  window.addEventListener('resize', handleWindowResize);
+  document.addEventListener('contextmenu', handleRightClick); 
